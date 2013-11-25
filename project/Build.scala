14e291457aec67167996092c289c8be46abc6022
rusabakumov <rusabakumov@gmail.com>
2013-11-25 14:26:27 +0400

    Fixed artifact name and bumped up scala and spray versions

--- project/Build.scala
+++ project/Build.scala
@@ -4,13 +4,13 @@ import Keys._
 object Build extends sbt.Build{
 
   lazy val proj = Project(
-    "WebSockets",
+    "spraywebsockets",
     file("."),
     settings =
       Defaults.defaultSettings ++ Seq(
-      organization  := "com.example",
+      organization  := "com.github.lihaoyi",
       version       := "0.1",
-      scalaVersion  := "2.10.2",
+      scalaVersion  := "2.10.3",
 
       resolvers ++= Seq(
         "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
@@ -18,7 +18,7 @@ object Build extends sbt.Build{
         "spray nightly"      at "http://nightlies.spray.io/"
       ),
       libraryDependencies ++= Seq(
-        "io.spray"            %   "spray-can"     % "1.2-RC3",
+        "io.spray"            %   "spray-can"     % "1.2-RC4",
         "com.typesafe.akka"   %%  "akka-actor"    % "2.2.3",
         "com.typesafe.akka"   %%  "akka-testkit"  % "2.2.3" % "test",
         "org.scalatest"       % "scalatest_2.10" % "2.0" % "test"
